6980ea70a0926d0bf5f7edd7275f136555d82c70
oqnunes <jefferson.mauricio17@gmail.com>
2020-11-13 23:12:34 +0000

    Add Typescript and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return(
+    response.json({
+      message: 'Hello World'
+    })
+  );
+});
+
+app.listen(3952);
