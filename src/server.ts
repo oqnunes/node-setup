9a1c570d2da6b2243dfb59337f2b037b95a355f8
oqnunes <jefferson.mauricio17@gmail.com>
2020-11-14 02:19:10 +0000

    End day

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import express from 'express';
+import express from 'express'
 
-import '@controllers/UsersController';
-
-const app = express();
+const app = express()
 
 app.get('/', (request, response) => {
-  return(
+  return (
     response.json({
       message: 'Hello World'
     })
-  );
-});
+  )
+})
 
-app.listen(3952);
+app.listen(3952)
